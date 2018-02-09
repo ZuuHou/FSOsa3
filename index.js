e9c3e87836f72610ac234ec29faff89a45b0cfb5
Juho Niittylä <juho.niittyla@helsinki.fi>
2018-02-09 10:37:22 +0200

    3.1-3.8

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 const bodyParser = require('body-parser')
 
 app.use(bodyParser.json())
+
+morgan.token('data', function (res, req) {
+    return JSON.stringify(req.body)
+})
+app.use(morgan(':method :url :data :status :res[content-length] - :response-time ms'))
 
 let persons = [
     {
